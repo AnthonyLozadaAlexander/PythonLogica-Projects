f98fb341b3989146a2359b1fe5b898007411a741
zDefcon <antholozadarequelme@gmail.com>
2025-11-21 01:45:40 -0500

    Refactorizar el manejo de la solución en EcuacionLineal.py para mejorar la legibilidad y el manejo de errores. Se agregó una verificación para determinar si la solución es un mensaje de error antes de imprimir el resultado.

--- Matematica/EcuacionLineal.py
+++ Matematica/EcuacionLineal.py
@@ -4,15 +4,25 @@ def calcularEcuacion(b, a):
     
     x = -(b) / a
     return x
+
 try:
+    
     print("---------------------------------------------------")
     print(("Resolver La Ecuacion Lineal De La Forma Ax + B = 0"))
     print("---------------------------------------------------")
     b = float(input("Ingrese el valor de B: "))
     a = float(input("Ingrese el valor de A: "))
     x = calcularEcuacion(b, a)
-    print("---------------------------------------------------")
-    print(f"La Solucion De La Ecuacion {a}x + {b} = 0 es x = {x}")
-    print("---------------------------------------------------")
+    
+    if(x is str):#{
+        print(x)
+    #}
+    else:#{
+        print("---------------------------------------------------")
+        print(f"La Solucion De La Ecuacion {a}x + {b} = 0 es x = {x}")
+        print("---------------------------------------------------")    
+    #}
+    
+    
 except ValueError:
     print("Error: Por favor ingrese un valor numérico válido para A y B.")
