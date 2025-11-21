7298235d8d43a5b686801992a00fad0d2c307b38
zDefcon <antholozadarequelme@gmail.com>
2025-11-21 01:06:03 -0500

    Agregar manejo de excepciones para la entrada de temperatura en el conversor de Celsius a Fahrenheit.

--- Logica/ConversorTemperatura.py
+++ Logica/ConversorTemperatura.py
@@ -2,10 +2,15 @@ def calcularFahrenheit(celsius):
     F = ((9/5) * celsius) + 32
     return F
 
-print("---------------------------------------------------")
-print("           Calculadora Celsius a Fahrenheit        ")
-print("---------------------------------------------------")
-celcius = float(input("Ingrese la temperatura en Celsius: "))
-fahrenheit = calcularFahrenheit(celcius)
-print(f"La Temperatura de {celcius}°C es igual a {fahrenheit}°F")
-print("---------------------------------------------------")
+try:
+    
+    print("---------------------------------------------------")
+    print("           Calculadora Celsius a Fahrenheit        ")
+    print("---------------------------------------------------")
+    celcius = float(input("Ingrese la temperatura en Celsius: "))
+    fahrenheit = calcularFahrenheit(celcius)
+    print(f"La Temperatura de {celcius}°C es igual a {fahrenheit}°F")
+    print("---------------------------------------------------")
+    
+except ValueError:
+    print("Error: Por favor ingrese un valor numérico válido para la temperatura.")
