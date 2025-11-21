7596cf18fdfad86933230e8fed63c324f1d24a26
zDefcon <antholozadarequelme@gmail.com>
2025-11-21 00:52:28 -0500

    Agregar manejo de excepciones para el ingreso de edad en el programa de saludo formateado.

--- Logica/SaludoFormateado.py
+++ Logica/SaludoFormateado.py
@@ -1,9 +1,18 @@
-print("--------------------------------------------")
-print("Bienvenido al programa de saludo formateado.")
-print("--------------------------------------------")
-nombre = input("Ingresa tu nombre: ")
-edad = input("Ingresa tu edad: ")
-print("--------------------------------------------")
-print(f"Hola, {nombre}! tienes {edad} años")
-print(f"El Proximo Año Tendras {(int(edad)+1)} años")
-print("--------------------------------------------")
+try :#{
+    print("--------------------------------------------")
+    print("Bienvenido al programa de saludo formateado.")
+    print("--------------------------------------------")
+    nombre = input("Ingresa tu nombre: ")
+    edad = input("Ingresa tu edad: ")
+    print("--------------------------------------------")
+    print(f"Hola, {nombre}! tienes {edad} años")
+    print(f"El Proximo Año Tendras {(int(edad)+1)} años")
+    print("--------------------------------------------")    
+#}
+except ValueError :#{
+    print("--------------------------------------------")
+    print(ValueError)
+    print("Error: Formato De Edad Incorrecta")
+    print("--------------------------------------------")
+#}
+
