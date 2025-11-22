bd2dcc7733e8980431045b54881656208aec2c57
zDefcon <antholozadarequelme@gmail.com>
2025-11-22 00:04:03 -0500

    Añadir el archivo AreaPerimetro.py con funciones para calcular el área y el perímetro de un círculo, incluyendo la constante pi y mensajes de presentación.

---
+++ Matematica/AreaPerimetro.py
@@ -0,0 +1,12 @@
+def calcularArea(pi, r):
+    area = pi * (r**2)
+    return area
+
+def calcularPerimetro(pi, r):
+    perimetro = 2*(pi * r)
+    return perimetro
+
+pi = 3.1416
+print("---------------------------------------------------")
+print("        Calculadora de Area y Perimetro            ")
+print("---------------------------------------------------")
