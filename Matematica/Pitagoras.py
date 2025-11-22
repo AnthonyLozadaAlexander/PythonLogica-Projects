ce76da455c561c57d20c0f112cbfbe2dd2140753
zDefcon <antholozadarequelme@gmail.com>
2025-11-21 23:27:35 -0500

    Añade cálculo de hipotenusa y validación de entrada
    Se ha importado el módulo `math` para realizar cálculos matemáticos.
    Se ha añadido la función `calcularP(a, b)` para calcular la hipotenusa
    usando el Teorema de Pitágoras.
    
    Se ha implementado la función `isNumber(n)` para validar si una cadena
    representa un número válido.
    
    Se ha añadido interacción con el usuario para ingresar los valores de
    los catetos, validarlos y calcular la hipotenusa si son correctos.
    Se incluyen mensajes en consola para guiar al usuario y manejar errores.

---
+++ Matematica/Pitagoras.py
@@ -0,0 +1,31 @@
+import math
+
+def calcularP(a, b):
+    c = math.sqrt(a**2 + b**2)
+    return c
+
+def isNumber(n):
+    regex = r'^-?\d+(\.\d+)?$'
+
+    if(re.match(regex, n)):
+        return True
+    else:
+        return False
+
+
+print("---------------------------------------------------")
+print("      Algoritmo Calcular Teorema De Pitagoras")
+print("---------------------------------------------------")
+a = input("Ingrese el valor del cateto a: ")
+b = input("Ingrese el valor del cateto b: ")
+print("---------------------------------------------------")
+
+if(isNumber(a) and isNumber(b)):
+    a = float(a)
+    b = float(b)
+    c = calcularP(a, b)
+    print("Catetos Ingresados Correctamente")
+else:
+    print("Error: Los Catetos Ingresados No Son Numeros Validos")
+
+print("---------------------------------------------------")
