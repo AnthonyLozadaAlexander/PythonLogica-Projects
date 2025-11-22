65209bb30cbec85cfd9fddb7b520b8767e989643
zDefcon <antholozadarequelme@gmail.com>
2025-11-22 00:08:17 -0500

    Refactorizar el código en SaludoFormateado.py para encapsular la lógica de saludo en una función, mejorando la legibilidad y la estructura del programa.

--- Logica/SaludoFormateado.py
+++ Logica/SaludoFormateado.py
@@ -1,13 +1,18 @@
+def saludo_formateado(nombre, edad):
+    print("--------------------------------------------")
+    print(f"Hola, {nombre}! tienes {edad} años")
+    print(f"El Proximo Año Tendras {(int(edad)+1)} años")
+    print("--------------------------------------------")
+
+
 try :#{
     print("--------------------------------------------")
     print("Bienvenido al programa de saludo formateado.")
     print("--------------------------------------------")
     nombre = input("Ingresa tu nombre: ")
     edad = input("Ingresa tu edad: ")
-    print("--------------------------------------------")
-    print(f"Hola, {nombre}! tienes {edad} años")
-    print(f"El Proximo Año Tendras {(int(edad)+1)} años")
-    print("--------------------------------------------")    
+    saludo_formateado(nombre, edad)
+        
 #}
 except ValueError :#{
     print("--------------------------------------------")
