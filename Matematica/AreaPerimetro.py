8fd49168357624c12fc0b840dda1d987c7c69cbd
zDefcon <antholozadarequelme@gmail.com>
2025-11-22 00:12:21 -0500

    Reorganizar el código en AreaPerimetro.py para mejorar la estructura y legibilidad, moviendo la entrada del radio y la lógica de selección de cálculo dentro de la estructura de control 'match'.

--- Matematica/AreaPerimetro.py
+++ Matematica/AreaPerimetro.py
@@ -10,4 +10,13 @@ pi = 3.1416
 print("---------------------------------------------------")
 print("        Calculadora de Area y Perimetro            ")
 print("---------------------------------------------------")
+r = float(input("Ingrese el valor del radio: "))
 opcion = input("Ingrese 'A' para calcular Area o 'P' para calcular Perimetro: ")
+
+match opcion.upper():
+    case 'A':
+        calcularArea(pi, r)
+    case 'P':
+        calcularPerimetro(pi, r)
+    case _:
+        print("Opcion No Valida")
