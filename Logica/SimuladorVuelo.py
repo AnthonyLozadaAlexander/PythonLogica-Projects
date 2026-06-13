3846fb14c1dfb784ed56b21e6b0b360d6e94b206
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-13 01:44:34 -0500

    Añadir el archivo SimuladorVuelo.py con la lógica para calcular el tiempo estimado para interceptar un asteroide, incluyendo validaciones de entrada y mensajes informativos.

---
+++ Logica/SimuladorVuelo.py
@@ -0,0 +1,40 @@
+def mostrarLinea():
+    print("==============================================================="  )
+
+
+mostrarLinea()
+print("                       Simulador de Vuelo")
+mostrarLinea()
+
+d = 0.0 #km
+v = 0.0 #km/h
+tMax = 4 #hras # si la sonda llega a tardar mas de 4 horas, el asteoride ya no se encontrara ahi
+
+print(f"  Tiempo Maximo Para Interceptar Al Asteoride: {tMax}\n")
+
+on = False
+while(on == False):
+    d = float(input("   Ingrese la distancia de la sonda al asteroide en km: "))
+
+    if(d <= 0):
+        print("Error: La distancia no puede ser negativa ni igual a cero")
+        continue # vuelve al inicio del while
+    else:
+        v = float(input("   Ingrese la velocidad de la sonda en km/h: "))
+        if(v <= 0):
+            print("Error: La velocidad no puede ser negativa ni igual a cero")
+            continue
+        else:
+            on = True
+
+t = d / v
+
+# .2f es para redondear a 2 decimales
+print(f"   Tiempo Estimado Para Interceptar Al Asteoride: {t} horas\n")
+
+if(t == tMax):
+    print("   La sonda llegara justo a tiempo para interceptar al asteroide")
+elif(t < tMax):
+    print("   La sonda llegara a tiempo para interceptar al asteoride")
+else:
+    print("   La sonda no llegara a tiempo para interceptar al asteroide")
