7bbda3163d584017c51a5aef1b0f53b6281978db
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-14 17:54:09 -0500

    Añadir el archivo PlanificadorProcesosCPU.py con la lógica para mostrar procesos en entrada y calcular el tiempo de espera total, incluyendo funciones para mostrar líneas y arreglos.

---
+++ Logica/PlanificadorProcesosCPU.py
@@ -0,0 +1,44 @@
+def mostrarLinea():
+    print("==============================================================="  )
+
+def menu(E):
+    mostrarLinea()
+    print("                 PLANIFICADOR DE PROCESOS ")    
+    mostrarLinea()
+    mostrarArreglo(E)
+
+def mostrarArreglo(E):
+    print("Procesos En Entrada")
+    for i in range(len(E)):
+        print(f"Proceso[{i}]: {E[i]}")
+        
+def calcularTiempoEsperaTotal(TE):
+    total = 0
+    for i in range(len(TE)):
+        total = total + TE[i]
+
+    return total
+
+E = [5, 3, 6] # arreglo de procesos
+TE = [0] * len(E) # asignarle la longitud al arreglo
+
+menu(E)
+
+countE = 0
+
+for i in range(len(E)):
+
+    if(i > 0 and i < len(E)):
+        countE =  countE + E[i-1]
+        TE[i] = countE
+    else:
+        TE[i] = countE
+
+print("\nTiempo De Espera")
+for i in range(len(TE)):
+    print(f"TE[{i}] = {TE[i]}")
+    
+
+total = calcularTiempoEsperaTotal(TE)
+print(f"\nTiempo De Espera Total: {total}")
+
