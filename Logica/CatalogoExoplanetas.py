b96b98a2a5c2181c1ad7d32cfdf99c114f4efede
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-20 04:25:24 -0500

    Añadir funciones para mostrar líneas y medir el tiempo de ejecución en CatalogoExoplanetas.py, mejorando la presentación y el seguimiento del rendimiento.

--- Logica/CatalogoExoplanetas.py
+++ Logica/CatalogoExoplanetas.py
@@ -1,3 +1,9 @@
+import time
+
+
+def mostrarLinea():
+    print("==============================================================="  )
+
 def ordenarM(M):
     for i in range(len(M) - 1):
         for j in range(len(M) - i - 1):
@@ -14,16 +20,24 @@ def mostrarArr(M):
     
 
 
+t_inicial = time.perf_counter()
 M = [8, 3, 5, 1]
 
+mostrarLinea()
 print(" Masa De Explanetas ")
+mostrarLinea()
 
 mostrarArr(M)
 ordenarM(M)
 
+mostrarLinea()
 print("\n Ordenando Masas De Exoplanetas ")
-
+mostrarLinea()
 mostrarArr(M)
+
+t_final = time.perf_counter()
+tiempoEjecucion = (t_final - t_inicial) * 1000
+print(f"Tiempo Ejecucion: {tiempoEjecucion: .4f} [Ms]")
 
 
 
