e17676dfae92f3b00b0c02a50263796527c78d86
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-19 13:27:05 -0500

    Agrega función de búsqueda binaria y método para imprimir vector
    Se implementa la función `busquedaBinaria` que permite buscar un elemento en un vector de manera eficiente. También se agrega la función `imprimirVector` para mostrar el contenido del vector en un formato legible. Esto mejora la funcionalidad del programa al permitir la búsqueda de datos en una lista predefinida.

---
+++ Logica/DatosBusquedaB.py
@@ -0,0 +1,35 @@
+﻿def imprimirVector(A):
+	print("[ ", end="")
+	for i in range(len(A)):
+		if (i < len(A) - 1):
+			print(f"{A[i]}, ", end="")
+		else:
+			print(f" {A[i]}", end="")
+	print(" ]", end="")
+
+def busquedaBinaria(A, T):
+	punteroIzq = 0  # [0]
+	punteroDer = (len(A) - 1)  # [8]
+
+	while (punteroIzq < punteroDer):
+		encontrado = False
+		medio = ((punteroIzq + punteroDer) // 2)
+		if (A[medio] == T):
+			encontrado = True
+		if (A[medio] < T):
+			punteroIzq = (medio + 1)
+		else:
+			punteroDer = (medio - 1)
+
+	return encontrado
+
+T = 42
+A = [ 3, 9, 14, 19, 25, 31, 42, 47, 53]
+print("Datos En La Base De Datos")
+imprimirVector(A)
+print(f"\nDato a Buscar En La Base De Datos: {T}")
+encontrado = busquedaBinaria(A, T)
+if(encontrado):
+	print(f"\nEl Dato {T} fue encontrado en la base de datos")
+else:
+	print(f"\nEl Dato {T} no fue encontrado en la base de datos")
