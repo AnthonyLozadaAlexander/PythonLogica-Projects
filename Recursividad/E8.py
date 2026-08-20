fef8d118539e315217113239d36df19d6b2047d0
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-20 11:15:48 -0500

    Agrega función para verificar si una palabra es un palíndromo
    Se implementa la función `esPalindromo` que utiliza recursión para determinar si una palabra es un palíndromo. Se incluyen ejemplos de uso con las palabras "flor" y "oso", mostrando el resultado en la consola.

---
+++ Recursividad/E8.py
@@ -0,0 +1,24 @@
+def esPalindromo(palabra: str, pointerLeft : int,  pointerRight : int) -> bool:
+	on: bool = True
+	if ((pointerLeft < pointerRight) and (on)):
+		if (palabra[pointerLeft] == palabra[pointerRight]):
+			on = esPalindromo(palabra, pointerLeft + 1, pointerRight - 1)
+		else:
+			on = False
+
+	return on
+
+palabra1: str = "flor"
+palabra2: str = "oso"
+esPalindromo1: bool = esPalindromo(palabra1, 0, len(palabra1) - 1)
+esPalindromo2: bool = esPalindromo(palabra2, 0, len(palabra2) - 1)
+
+if(esPalindromo1):
+	print(f"\n La palabra {palabra1} es palindromo")
+else:
+	print(f"\n La palabra {palabra1} no es palindromo")
+
+if(esPalindromo2):
+	print(f"\n La palabra {palabra2} es palindromo")
+else:
+	print(f"\n La palabra {palabra2} no es palindromo")
