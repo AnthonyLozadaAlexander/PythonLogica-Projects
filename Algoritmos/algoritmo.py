3d08cc8ec7d28f80174e6c69aab5be49406e1358
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-20 01:02:49 -0500

    Agrega funciones para imprimir vectores y matrices
    Se implementan las funciones 'imprimirVector' e 'imprimirMatriz' para facilitar la visualización de estructuras de datos en formato legible. Estas funciones permiten imprimir arreglos y matrices de manera estructurada, mejorando la depuración y el análisis de datos en el código.

---
+++ Algoritmos/algoritmo.py
@@ -0,0 +1,19 @@
+﻿def imprimirVector(arr):
+	print("[ ", end="")
+	for i in range(len(arr)):
+		if (i < len(arr) - 1):
+			print(f"{arr[i]}, ", end="")
+		else:
+			print(f" {arr[i]}", end="")
+	print(" ]", end="")
+
+def imprimirMatriz(M):
+	print("[", end="")
+	for i in range(len(M)):
+		for j in range(len(M[i])):
+			print(f" {M[i][j]} ", end="")
+
+		if (i < len(M) - 1):
+			print("\n")
+
+	print("]", end="")
