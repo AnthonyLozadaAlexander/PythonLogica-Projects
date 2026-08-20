c2479e1351050da5bf02fa107acd2a8b31bba931
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-20 00:48:03 -0500

    Agrega espacio en blanco para mejorar la legibilidad del código

--- Logica/Hipotenusa.py
+++ Logica/Hipotenusa.py
@@ -10,6 +10,7 @@ print("---------------------------------------------------")
 a = float(input("Ingrese el primer cateto: "))
 b = float(input("Ingrese el segundo cateto: "))
 hipotenusa = calcularHipotenusa(a, b)
+
 print("---------------------------------------------------")
 print(f"La hipotenusa de un triangulo con catetos {a} y {b} es igual a {hipotenusa}")
 print("---------------------------------------------------")
