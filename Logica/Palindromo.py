56b1edf20376f3281b809eee2000de9c2a34e7e2
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-20 00:47:50 -0500

    Agrega función para determinar si una palabra es un palíndromo
    Implementa el algoritmo que verifica si una palabra se lee igual de izquierda a derecha que de derecha a izquierda. Incluye un ejemplo de uso en la salida del programa.

---
+++ Logica/Palindromo.py
@@ -0,0 +1,24 @@
+﻿def determinarPalindromo(palabra):
+	pointerLeft = 0
+	pointerRight = len(palabra) - 1
+	esPalindromo = True
+
+	while((pointerLeft <  pointerRight) and (esPalindromo)):
+		if(palabra[pointerLeft] == palabra[pointerRight]):
+			pointerLeft = pointerLeft + 1
+			pointerRight  = pointerRight - 1
+		else:
+		    esPalindromo = False
+
+	return esPalindromo
+
+
+print("Algoritmo Para Determinar Si Una Palabra Es Palindromo")
+palabra = "radar"
+print(f"\n Palabra a Evaluar: {palabra}")
+
+esPalindromo = determinarPalindromo(palabra)
+if(esPalindromo):
+	print(f"\n La Palabra {palabra} es un Palindromo")
+else:
+	print(f"\n La Palabra {palabra} no es un Palindromo")
