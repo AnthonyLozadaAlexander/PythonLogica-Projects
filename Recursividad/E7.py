c93e189bd33c1bb853490f771a566b899b569870
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-20 03:10:24 -0500

    Agrega algoritmo recursivo para encontrar el valor máximo de un arreglo
    Implementa la función `valorMaximo` que utiliza recursión para determinar el valor máximo en un arreglo positivo. Se incluye un ejemplo de uso con un arreglo predefinido.

---
+++ Recursividad/E7.py
@@ -0,0 +1,23 @@
+#Algoritmo Recursivo para encontrar el valor maximo de un arreglo positivo
+def valorMaximo(Arr, Copia, count):
+    
+    result = 0
+    aux1 = 0
+    
+    if(count <= len(Arr) -  1):
+                aux1 = Arr[count]
+                count = count + 1    
+                result = valorMaximo(Arr, Copia, count)
+                
+    
+    aux2 = Copia[0]
+    if(aux2 <= aux1):
+        result = aux1
+        Copia[0] = aux1
+        
+    return result
+
+A = [6, 0, 9, 4, 7]
+Copia = A.copy()
+print(f"El valor maximo del arreglo es: {valorMaximo(A, Copia, 0)}")
+
