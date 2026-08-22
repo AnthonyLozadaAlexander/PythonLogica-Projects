27a21004ffb006dcf378bfc56e21f9b61eb66094
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-22 03:59:07 -0500

    Elimina archivos de configuración innecesarios y archivos de bloqueo del proyecto

--- Algoritmos/algoritmo.py
+++ Algoritmos/algoritmo.py
@@ -1,23 +1,25 @@
 ﻿from typing import TypeVar
 
-T = TypeVar('T')
+T = TypeVar("T")
 
-def imprimirVector(arr : list[T]) -> None:
-	print("[ ", end="")
-	for i in range(len(arr)):
-		if (i < len(arr) - 1):
-			print(f"{arr[i]}, ", end="")
-		else:
-			print(f" {arr[i]}", end="")
-	print(" ]", end="")
 
-def imprimirMatriz(M : list[list[T]]) -> None:
-	print("[", end="")
-	for i in range(len(M)):
-		for j in range(len(M[i])):
-			print(f" {M[i][j]} ", end="")
+def imprimirVector(arr: list[T]) -> None:
+    print("[ ", end="")
+    for i in range(len(arr)):
+        if i < len(arr) - 1:
+            print(f"{arr[i]}, ", end="")
+        else:
+            print(f"{arr[i]}", end="")
+    print(" ]", end="")
 
-		if (i < len(M) - 1):
-			print("\n")
 
-	print("]", end="")
+def imprimirMatriz(M: list[list[T]]) -> None:
+    print("[", end="")
+    for i in range(len(M)):
+        for j in range(len(M[i])):
+            print(f" {M[i][j]} ", end="")
+
+        if i < len(M) - 1:
+            print("\n")
+
+    print("]", end="")
---
+++ Recursividad/E9.py
@@ -0,0 +1,7 @@
+from Algoritmos import algoritmo
+
+# Algoritmo Busqueda Binaria Recursiva
+
+
+A: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+algoritmo.imprimirVector(A)
