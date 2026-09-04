c7766be58822228e81cfcc55900ccefadf1a51bb
zDefcon21 <antholozadarequelme@gmail.com>
2026-09-04 18:21:12 -0500

    Corrige condición de búsqueda en función de búsqueda binaria recursiva
    Se ajusta la condición de la función `busquedaBinariaR` para permitir la búsqueda del valor en el índice derecho. Además, se actualizan los valores de prueba en el arreglo y el valor a buscar.

--- Recursividad/E9.py
+++ Recursividad/E9.py
@@ -1,27 +1,26 @@
 from Algoritmos import algoritmo
 
-
 # Algoritmo Busqueda Binaria Recursiva
 def busquedaBinariaR(
     A: list[int], valor: int, left: int, right: int, encontrado: bool
 ) -> bool:
-    if left < right and not encontrado:
+    if left <= right and not encontrado:
         mid: int = (left + right) // 2
         if A[mid] == valor:
             encontrado = True
         elif A[mid] < valor:
             left = mid + 1
+            encontrado = busquedaBinariaR(A, valor, left, right, encontrado)
         else:
             right = mid - 1
-
-        encontrado = busquedaBinariaR(A, valor, left, right, encontrado)
+            encontrado = busquedaBinariaR(A, valor, left, right, encontrado)
 
     return encontrado
 
 
 on: bool = False
-A: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-valor: int = 3
+A: list[int] = [2, 4, 6, 8, 10]
+valor: int = 8
 algoritmo.imprimirVector(A)
 encontrado: bool = busquedaBinariaR(A, valor, 0, len(A) - 1, on)
 print(f"\n\nValor {valor} Encontrado: ", encontrado)
