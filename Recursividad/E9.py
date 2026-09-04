9d0b43fb2c3b9b44ecea2082af7c90bc84b69811
zDefcon21 <antholozadarequelme@gmail.com>
2026-09-04 17:21:09 -0500

    Agrega función de búsqueda binaria recursiva
    Implementa la función busquedaBinariaR para realizar búsquedas en una lista de enteros. La función devuelve un valor booleano indicando si el elemento buscado fue encontrado. Se actualiza el archivo E9.py para incluir esta nueva funcionalidad.

--- Recursividad/E9.py
+++ Recursividad/E9.py
@@ -1,7 +1,27 @@
 from Algoritmos import algoritmo
 
+
 # Algoritmo Busqueda Binaria Recursiva
+def busquedaBinariaR(
+    A: list[int], valor: int, left: int, right: int, encontrado: bool
+) -> bool:
+    if left < right and not encontrado:
+        mid: int = (left + right) // 2
+        if A[mid] == valor:
+            encontrado = True
+        elif A[mid] < valor:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+        encontrado = busquedaBinariaR(A, valor, left, right, encontrado)
+
+    return encontrado
 
 
+on: bool = False
 A: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+valor: int = 3
 algoritmo.imprimirVector(A)
+encontrado: bool = busquedaBinariaR(A, valor, 0, len(A) - 1, on)
+print(f"\n\nValor {valor} Encontrado: ", encontrado)
