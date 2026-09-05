6fa160c598ce9a4aa34a7bc4008177f91722a16f
zDefcon21 <antholozadarequelme@gmail.com>
2026-09-04 19:04:32 -0500

    Agrega función para invertir un arreglo
    Implementa la función `inversionArreglo` que invierte un arreglo de enteros de manera recursiva. La función modifica el arreglo en su lugar y devuelve un booleano que indica si la inversión fue exitosa. Se incluye un ejemplo de uso con un arreglo de prueba.

---
+++ Recursividad/E10.py
@@ -0,0 +1,17 @@
+﻿def inversionArreglo(arr: list[int], left: int, right: int, invertido: bool) -> bool:
+	if(left >= right):
+		invertido = True
+	else:
+			guardar : int = arr[left]
+			arr[left] = arr[right]
+			arr[right] = guardar
+			invertido =	inversionArreglo(arr, left + 1, right - 1, invertido)
+
+	return invertido
+
+arr : list[int] = [1, 2, 3, 4, 5]
+invertido : bool = False
+print(arr)
+invertido = inversionArreglo(arr, 0, len(arr) - 1, invertido)
+print(invertido)
+print(arr)
